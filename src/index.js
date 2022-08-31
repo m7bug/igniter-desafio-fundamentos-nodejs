5fa9766e0259f081338bab6d78b7dbb19e65d8e0
Matheus Monteiro <matheus.stack.20@hotmail.com>
2022-08-30 22:28:42 -0300

    Iniciando desafio: > [x] Cadastrar um usuário > [x] Listar os todos do usuário

--- src/index.js
+++ src/index.js
@@ -1,40 +1,62 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require("uuid");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find((user) => user.username === username);
+
+  if (!user) {
+    return response.status(404).json({ message: "User not found" });
+  }
+
+  request.user = user;
+
+  return next();
 }
 
-app.post('/users', (request, response) => {
+app.post("/users", (request, response) => {
+  const { name, username } = request.body;
+
+  users.push({
+    id: uuidv4(),
+    name: name,
+    username: username,
+    todos: [],
+  });
+
+  return response.status(201).send();
+});
+
+app.use(checksExistsUserAccount);
+
+app.get("/todos", (request, response) => {
+  const { user } = request;
+  return response.status(200).send(user.todos);
+});
+
+app.post("/todos", (request, response) => {
   // Complete aqui
 });
 
-app.get('/todos', checksExistsUserAccount, (request, response) => {
+app.put("/todos/:id", (request, response) => {
   // Complete aqui
 });
 
-app.post('/todos', checksExistsUserAccount, (request, response) => {
+app.patch("/todos/:id/done", (request, response) => {
   // Complete aqui
 });
 
-app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
-});
-
-app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
-});
-
-app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
+app.delete("/todos/:id", (request, response) => {
   // Complete aqui
 });
 
